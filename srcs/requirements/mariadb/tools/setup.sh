11a77065949cbf75efe3779e1ad18a937c92edc5
adidion19 <didionantoine@gmail.com>
2022-05-20 16:37:24 +0200

    Make

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -13,4 +13,4 @@ service mysql stop
 fi
 #sleep 5
 echo "ALTER USER 'root'@'localhost'IDENTIFIED BY '$DB_PASSWORD';" > /var/lib/mysql/pass-reset
-mysql_safe -init-file=/var/lib/mysql/pass-reset
+mysqld_safe -init-file=/var/lib/mysql/pass-reset
